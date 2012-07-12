890bfb171f27d580833fd8d3cf344c3df542ba14
SCarr Linode <scarr025@gmail.com>
2012-07-12 10:11:23 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dataway::Application.config.secret_token = 'c747584ed57c413dc97e06df336c475b2ceb9da0c4174e4dc75ac9f20a04c77484d05d37ebd91795bf525998fd63052bdf70fa4cb7b90fd413e9375c4641efca'
