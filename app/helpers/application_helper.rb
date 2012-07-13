d6819bc9f6c0f8d398ce0c74f1027a4b22bc61f8
SCarr Linode <scarr025@gmail.com>
2012-07-13 21:44:55 +0000

    Init converging devise log_in page with own log_in page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,19 @@ module ApplicationHelper
 		else
 			"#{base_title} | #{page_title}"
 		end
-	end	
+	end
+	
+	# Include Devise resource control in non-Devise controllers
+	def resource_name
+    		:user
+  	end
+
+  	def resource
+		@resource ||= User.new
+	end
+
+    	def devise_mapping
+		@devise_mapping ||= Devise.mappings[:user]
+	end
+
 end
