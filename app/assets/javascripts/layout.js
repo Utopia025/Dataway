c70ae2a2f539f62a59bf86627453c750a3760e38
SCarr Linode <scarr025@gmail.com>
2012-07-27 15:32:46 +0000

    Have correct jQuery header-color-changer code, but not working

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,16 +1,16 @@
+<script type="text/javascript" src="jquery.js">
+<script type="text/javascript">
+$j = jQuery.noConflict();
+
+$j(document).ready(function() {
+	// Changes the color of the header button based on what page is currently presented
+	$j(".header-button").click(function() {
+		$j(".header-button").removeClass().addClass("header-button");
+		$j(this).addClass("active");
+	});	
+		
+});
 
 
-
-
-
-
-// Changes the color of the header button based on what page is currently presented
-function buttonColorChange(self)
-{
-	document.getElementByTagName("header-button").className = "header-button";
-	document.getElementById(self).className += " active";
-}
-
-
-
-
+</script>
+</script>
