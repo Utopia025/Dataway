a24d48a0a4ce9b7f2d1624fdef3a9c374d8110b6
SCarr Linode <scarr025@gmail.com>
2012-09-11 16:56:12 +0000

    1-Fix Devise Routing, saving pre-RPX DB implementation

--- app/assets/javascripts/layout.js
+++ app/assets/javascripts/layout.js
@@ -1,15 +1,10 @@
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
-<script type="text/javascript">
-$j = jQuery.noConflict();
-
-$j(document).ready(function() {
+$(document).ready(function() {
 	// Changes the color of the header button based on what page is currently presented
-	$j(".header-button").click(function() {
-		$j(".header-button").removeClass().addClass("header-button");
-		$j(this).addClass("active");
+	$(".header-button").click(function() {
+		$(".header-button").removeClass().addClass("header-button");
+		$(this).addClass("active");
 	});	
 		
 });
 
 
-</script>
